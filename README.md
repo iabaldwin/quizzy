2c22702c7102835f760f57bddef912156057cb03
Ian Baldwin <ian@iabaldwin.com>
2018-03-05 11:52:42 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Short coding puzzles
+
+This is a small repository of coding puzzles. To initialize:
+
+```
+$ git clone git@github.com:PointOneNav/quizzy.git
+```
+
+See the Makefile for a descriptrion of each of the problems.
---
+++ entropy.cc
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <iostream>
+#include <fstream>
+
+template <typename NumericType, int Discretization>
+NumericType entropy(const std::string& fileDescriptor) {
+  throw std::runtime_error("Implement me!");
+}
+
+int main() {
+  /*
+   *Print out the entropy of a probability distribution over 8 discrete events,
+   with event occurrences given by a random source of information. 
+   */
+  //std::cout << entropy<float, 8>("/dev/random") << std::endl;
+  /*
+   *Note: this is a suggested function signature, feel free to implement
+   whatever you see fit!
+   */
+}
