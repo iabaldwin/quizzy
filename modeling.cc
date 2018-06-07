2821598607e5ae7f97c99159ea0c2642d6fa97de
liwenlin <linliwen123@gmail.com>
2018-06-06 18:47:39 -0700

    done

--- modeling.cc
+++ modeling.cc
@@ -2,8 +2,11 @@
 #include <cmath>
 #include <array>
 
+using std::array;
+
 constexpr int kDimensions = 2;
 constexpr int kDataPoints = 20;
+constexpr int n_quad_param = 3;
 constexpr std::array<std::array<float, kDimensions>, kDataPoints> points = {{{3.8686992545234347, 1.3559682753431659},
                                                                              {0.5180993375153686, 5.117486709676035},
                                                                              {3.5386436080165806, 1.5271125008781592},
@@ -24,6 +27,108 @@ constexpr std::array<std::array<float, kDimensions>, kDataPoints> points = {{{3.
                                                                              {4.479413884955205, 1.1542019806354862},
                                                                              {3.0128605724511037, 1.8897446209038709},
                                                                              {1.9357873494944466, 2.9778798335036534}}};
+// Simple version of 3x3 matrix inversion
+int inverse_3x3(const array<array<float, 3>, 3> &src,
+                 array<array<float, 3>, 3> &dst) {
+    float det = +src[0][0]*(src[1][1]*src[2][2]-src[2][1]*src[1][2])
+                -src[0][1]*(src[1][0]*src[2][2]-src[1][2]*src[2][0])
+                +src[0][2]*(src[1][0]*src[2][1]-src[1][1]*src[2][0]);
+    if (det == 0)
+        return 1;
+    
+    double invdet = 1/det;
+    dst[0][0] =  (src[1][1]*src[2][2]-src[2][1]*src[1][2])*invdet;
+    dst[1][0] = -(src[0][1]*src[2][2]-src[0][2]*src[2][1])*invdet;
+    dst[2][0] =  (src[0][1]*src[1][2]-src[0][2]*src[1][1])*invdet;
+    dst[0][1] = -(src[1][0]*src[2][2]-src[1][2]*src[2][0])*invdet;
+    dst[1][1] =  (src[0][0]*src[2][2]-src[0][2]*src[2][0])*invdet;
+    dst[2][1] = -(src[0][0]*src[1][2]-src[1][0]*src[0][2])*invdet;
+    dst[0][2] =  (src[1][0]*src[2][1]-src[2][0]*src[1][1])*invdet;
+    dst[1][2] = -(src[0][0]*src[2][1]-src[2][0]*src[0][1])*invdet;
+    dst[2][2] =  (src[0][0]*src[1][1]-src[1][0]*src[0][1])*invdet;
+    
+    return 0;
+}
+
+// I plot out the point distribution, and found that quadratic model is good enough to
+// describe the relationship.
+// So I will use second order of polynomial regression model:
+// y = a0 + a1*x + a2*x^2
+// The result will be 3x1 vector which are the coefficients: [a0, a1, a2];
+// So the problem becomes to solve the linear equation of: Y = X*a, where
+// Y = [y0, y1, ... yn], a = [a0, a1, a2],
+// X = [1 x1 x1^2]
+//     [1 x2 x2^2]
+//     [...      ]
+//     [1 x2 xn^2]
+array<float, n_quad_param>
+model(const std::array<std::array<float, kDimensions>, kDataPoints> points) {
+    // Will not build X matrix directly in case too much memory
+    // comsumption with big input of points; instead, I build A, B matrix
+    // as below to solve this linear equation:
+    // A = (Xt*X) = [n         sum(xi)   sum(xi^2)]
+    //              [sum(xi)   sum(xi^2) sum(xi^3)]
+    //              [sum(xi^2) sum(xi^3) sum(xi^4)]
+    // B = (Xt*Y) = [sum(yi)     ]
+    //              [sum(xi*yi)  ]
+    //              [sum(xi^2*yi)]
+    array<array<float, n_quad_param>, n_quad_param> A = {0.f};
+    array<float, n_quad_param> B = {0};
+    
+    A[0][0] = kDataPoints;
+    float x, x2, x3, x4, y;
+    for (int i = 0; i < kDataPoints; i++) {
+        x = points[i][0];
+        x2 = x*x; x3 = x2*x; x4 = x2*x2;
+        y = points[i][1];
+        A[0][1] += x;
+        A[0][2] += x2;
+        A[1][2] += x3;
+        A[2][2] += x4;
+        
+        B[0] += y;
+        B[1] += x*y;
+        B[2] += x2*y;
+    }
+    A[1][1] = A[0][2];
+    for (int i = 0; i < n_quad_param; i++) {
+        for (int j = i+1; j < n_quad_param; j++) {
+            A[j][i] = A[i][j];
+        }
+    }
+    
+    // Get A inverse
+    array<array<float, n_quad_param>, n_quad_param> A_inv;
+    int ret = inverse_3x3(A, A_inv);
+    if (ret) {
+        std::cout << "observation matrix can not be inversed, program quits" << std::endl;
+        exit(1);
+    }
+    
+    // Get solution
+    array<float, n_quad_param> X = {0.f};
+    for (int i = 0; i < n_quad_param; i++) {
+        for (int j = 0; j < n_quad_param; j++) {
+            X[i] += A_inv[i][j]*B[j];
+        }
+    }
+    
+    // Get residual to see if it is small enough
+    float sum_r2 = 0;
+    for (int i = 0; i < kDataPoints; i++) {
+        x = points[i][0];
+        y = points[i][1];
+        float dy = X[0] + X[1]*x + X[2]*x*x - y;
+        sum_r2 += dy*dy;
+    }
+
+    std::cout << "Estimated parameters (a0, a1, a2): "
+              << X[0] << ", " << X[1] << ", " << X[2] << std::endl;
+    std::cout << "Sum of square of residual: " << sum_r2 << std::endl;
+
+    
+    return X;
+}
 
 
 int main() {
@@ -33,6 +138,9 @@ int main() {
    print out coefficients!
    */
   //std::cout << model(points) << std::endl;
+    array<float, n_quad_param> X = model(points);
+    
+    
   /*
    *Note: this is a suggested function signature, feel free to implement
    whatever you see fit!
--- strings.cc
+++ strings.cc
@@ -2,20 +2,56 @@
 #include <fstream>
 #include <vector>
 #include <assert.h>
+#include <string>
+
+using std::string;
+using std::vector;
+using std::ifstream;
+
+// Low case or upper case??
+
+bool isPalindromic(string s) {
+    int front = 0;
+    int end = s.length()-1;
+    while (front < end) {
+        if (s[front] != s[end])
+            return false;
+        
+        front++;
+        end--;
+    }
+    
+    return true;
+}
+
+vector<string> readWords(const string &fp) {
+    ifstream ifs(fp);
+    if (!ifs.is_open())
+        return {};
+    vector<string> res;
+    string line;
+    while (std::getline(ifs, line)) {
+        res.push_back(line);
+    }
+    
+    ifs.close();
+    return res;
+}
 
 int main() {
   /*
    *Read in the system dictionary, and print out the number of palindromic words
    */
-  //const auto words = readWords("/usr/share/dict/words");
+    const auto words = readWords("/usr/share/dict/words");
   /*
    *Print out the number of palindromic words in this container
    */
-  //std::cout << std::count_if(words.begin(),
-                             //words.end(),
-                             //[](const std::string & word) {
-                              //return isPalindromic(word);
-                              //}) << std::endl;;
+    //vector<string> words = {"ab", "aa"};
+    std::cout << std::count_if(words.begin(),
+                             words.end(),
+                             [](const std::string & word) {
+                              return isPalindromic(word);
+                              }) << std::endl;;
   /*
    *Note: this is a suggested function signature, feel free to implement
    whatever you see fit!
