6e6b94b30f594f220d142a6a75b4b8423dd303c6
Jordan Tritell <jtritell@gmail.com>
2018-06-16 17:23:06 -0700

    Completed quizzy

--- strings.cc
+++ strings.cc
@@ -2,22 +2,71 @@
 #include <fstream>
 #include <vector>
 #include <assert.h>
+#include <vector>
 
-int main() {
-  /*
-   *Read in the system dictionary, and print out the number of palindromic words
-   */
-  //const auto words = readWords("/usr/share/dict/words");
-  /*
-   *Print out the number of palindromic words in this container
-   */
-  //std::cout << std::count_if(words.begin(),
-                             //words.end(),
-                             //[](const std::string & word) {
-                              //return isPalindromic(word);
-                              //}) << std::endl;;
-  /*
-   *Note: this is a suggested function signature, feel free to implement
-   whatever you see fit!
-   */
+using namespace std;
+
+bool isPalindrone(char* str, int len)
+{
+    if (len == 0) return false;
+    for (int i = 0; i<len / 2; ++i)
+    {
+        if (str[i] != str[len - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int numPalindromes(string filename)
+{
+    ifstream inFile(filename.c_str(), ifstream::in);
+
+    if (!inFile.good())
+    {
+        cout << "Unable to access file: " << filename.c_str() << endl;
+        return 0;
+    }
+
+    char buffer[64];
+    int len = 0;
+    char ch;
+
+    int numPalindromes = 0;
+
+    while (inFile.good())
+    {
+        ch = inFile.get();
+        if (isspace(ch) || ch == '\n' || ch == '\0' || ch == -1)
+        {
+            if (len>0)
+            {
+                numPalindromes += isPalindrone(buffer, len) ? 1 : 0;
+                len = 0;
+            }
+        }
+        else
+        {
+            buffer[len++] = ch;
+        }
+    }
+    //handle last buffer
+    numPalindromes += isPalindrone(buffer, len) ? 1 : 0;
+
+    inFile.close();
+    return numPalindromes;
+}
+
+
+
+int main(int argc, char** argv)
+{
+    string filename = "/usr/share/dict/words";
+    if (argc >1)
+    {
+        filename = string(argv[1]);
+    }
+    cout << numPalindromes(filename);
 }
