98ebe3810d6b921163bb2347d349e046243480e0
Granger Lang <glang@users.noreply.github.com>
2018-06-16 23:33:57 -0700

    Solution for strings.cc

--- strings.cc
+++ strings.cc
@@ -1,23 +1,41 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 #include <assert.h>
 
+bool isPalindromic(const std::string& word) {
+  for (int i = 0; i < word.size()/2; ++i) {
+    if (word[i] != word[word.size() - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int numberOfPalindromes(const std::string path) {
+  std::ifstream file;
+  std::string word;
+  int numPals = 0;
+
+  file.open(path);
+
+  while (file >> word) {
+    if (isPalindromic(word)) {
+      ++numPals;
+    }
+  }
+
+  file.close();
+
+  return numPals;
+}
+
 int main() {
-  /*
-   *Read in the system dictionary, and print out the number of palindromic words
-   */
-  //const auto words = readWords("/usr/share/dict/words");
-  /*
-   *Print out the number of palindromic words in this container
-   */
-  //std::cout << std::count_if(words.begin(),
-                             //words.end(),
-                             //[](const std::string & word) {
-                              //return isPalindromic(word);
-                              //}) << std::endl;;
-  /*
-   *Note: this is a suggested function signature, feel free to implement
-   whatever you see fit!
-   */
+  std::string path;
+  std::cout << "Enter the path containing the dictionary:" << std::endl;
+  std::cin >> path;
+  std::cout << "There are " << numberOfPalindromes(path) << " palindromes in the dictionary." << std::endl;
+  return 0;
 }
