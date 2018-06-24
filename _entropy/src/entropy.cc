57a06fe806eea0ff1887b03f4d5a7c5b0ebb2095
Sergio Valero <esevalero@gmail.com>
2018-06-24 02:40:32 +0100

    strings.cc is returning correctly the palindromic words. entropy.cc reads source.

--- _entropy/src/entropy.cc
+++ _entropy/src/entropy.cc
@@ -1,21 +1,109 @@
+
+//
+// \file    entropy.cc
+// \author  Sergio Valero, esevalero@gmail.com
+// \date    21.06.2018
+//
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// \brief:  THis program Compute an estimate of the discrete entropy of a     //
+//          given source of random information (e.g.  "/dev/random"). Ensure  //
+//          that the random source provides integers ranging from [0,128],    //
+//          and assume that the input probability space is divided into 16    //
+//          events [0,...,8) -> 0, [8,...16) -> 1, etc. Report answer in bits.//
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
 #include <cmath>
 #include <iostream>
 #include <fstream>
+#include <random>
+#include <sys/stat.h>
+#include <assert.h>
+
 
 template <typename NumericType, int Discretization>
 NumericType entropy(const std::string& fileDescriptor) {
   throw std::runtime_error("Implement me!");
 }
 
+//------------------------------------------------------------------------------
+// In line Functions
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// \brief   File sanity check inline function
+//
+inline bool isFile (
+    const std::string&  name        //< String with the name of the file to check
+){
+    struct stat buffer;
+    return (stat (name.c_str(), &buffer) == 0);
+}
+
+
+//------------------------------------------------------------------------------
+// \brief       Insert path to the random source
+//
+// \return      String container to the random source path
+//
+const std::string inserPathToRandomSource(
+    void
+){
+    std::string path;
+    std::cout << "_____________________________________________________" << std::endl << std::endl;
+    std::cout << "Hey!" << std::endl;
+    std::cout << "I can estimate the entropy of a random source! " << std::endl << std::endl ;
+    std::cout << "Could you please insert the path to your random source? " << std::endl;
+    std::cout << "      1. I can deal with .csv coma separated files, " << std::endl;
+    std::cout << "      2. So, please ensure you feed me with csv files! " << std::endl << std::endl;
+    std::cout << "*****************************************************" << std::endl;
+    std::cout << "Insert Random Source File: " ;
+    std::cin >> path;
+
+    bool fileOK = isFile(path);
+
+    if (!fileOK){
+        std::cout << "Error: The File doesn´t exist." << std::endl;
+        assert( fileOK && "Error: The File doesn´t exist.") ;
+    }
+
+    bool isCSV = false;
+    if(path.substr(path.find_last_of(".") + 1) == "csv")
+        isCSV = true;
+
+    if (!isCSV){
+        std::cout << "Error: no .CSV file." << std::endl;
+        assert( isCSV && "Error: no .CSV file") ;
+    }
+
+    return path;
+}
+
+
+
+//------------------------------------------------------------------------------
+// Main
+//
 int main() {
-  /*
-   *Print out the entropy of a probability distribution over 8 discrete events,
-   with event occurrences given by a random source of information.
-   */
-  // std::cout << entropy<float, 8>("/dev/random") << std::endl;
-  std::cout << "Hola" << std::endl;
-  /*
-   *Note: this is a suggested function signature, feel free to implement
-   whatever you see fit!
-   */
+
+    // Print out the entropy of a probability distribution over 8 discrete events,
+    // with event occurrences given by a random source of information.
+
+    // std::cout << entropy<float, 8>("/dev/random") << std::endl;
+
+    std::string path = inserPathToRandomSource();
+
+    std::ifstream file ( path ); // declare file stream: http://www.cplusplus.com/reference/iostream/ifstream/
+    std::string value;
+    while ( file.good() )
+    {
+         getline ( file, value, ',' ); // read a string until next comma: http://www.cplusplus.com/reference/string/getline/
+         std::cout << value; // display value removing the first and the last character from it
+    }
+
 }
--- _strings/src/strings.cc
+++ _strings/src/strings.cc
@@ -1,24 +1,140 @@
+
+//
+// \file    strings.cc
+// \author  Sergio Valero, esevalero@gmail.com
+// \date    21.06.2018
+//
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// \brief:  This program that takes a path to a file containing a list        //
+//          of words (e.g. "/usr/share/dict) and count the number of          //
+//          palindromic words in the list. A palindromic word (for this       //
+//          problem) is a word that reads the same in the forwards and reverse//
+//          order, and is case-sensitive (i.e. Z != z). Report total count of //
+//          palindromic words                                                 //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <assert.h>
+#include <sys/stat.h>
+#include <algorithm>
 
+
+//------------------------------------------------------------------------------
+// In line Functions
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// \brief   File sanity check inline function
+//
+inline bool isFile (
+    const std::string&  name        //< String with the name of the file to check
+){
+    struct stat buffer;
+    return (stat (name.c_str(), &buffer) == 0);
+}
+
+//------------------------------------------------------------------------------
+// \brief       Insert path of the text file to analize
+//
+// \return      String container to the file path
+//
+const std::string inserPathToFile(
+    void
+){
+    std::string path;
+    std::cout << "_____________________________________________________" << std::endl << std::endl;
+    std::cout << "Hey!" << std::endl;
+    std::cout << "I can detect the palindromic words in a text! " << std::endl << std::endl ;
+    std::cout << "Could you please insert the path to your file? " << std::endl;
+    std::cout << "*****************************************************" << std::endl;
+    std::cout << "Insert the path to your file: " ;
+    std::cin >> path;
+    std::cout << std::endl;
+
+    bool fileOK = isFile(path);
+
+    if (!fileOK){
+        std::cout << "Error: The File doesn´t exist." << std::endl;
+        assert( fileOK && "Error: The File doesn´t exist.") ;
+    }
+
+    return path;
+}
+
+
+//------------------------------------------------------------------------------
+// \brief   this function take a text file as a input and return a vector of
+//          strings with al the words one by one.
+//
+const auto readWords(
+    std::string filePath    //< String with the path to the file
+){
+    std::vector<std::string> words;
+    std::string str;
+
+    std::ifstream fin(filePath);
+
+    for (int i = 0; (fin >> str); ++i)
+        words.push_back(str);
+
+    fin.close();
+
+    return words;
+}
+
+
+//------------------------------------------------------------------------------
+// \brief   Detect if a word is palindromic
+//
+bool isPalindromic(
+    std::string word    //< word to analize as a palindromic
+){
+    int start = 0,
+    int end   = word.length() - 1;
+    while (start < end) {
+        if ( std::toupper(word[start++]) != std::toupper(word[end--]) )
+            return false;
+    }
+
+    return true;
+}
+
+
+
+//------------------------------------------------------------------------------
+// Main
+//
 int main() {
-  /*
-   *Read in the system dictionary, and print out the number of palindromic words
-   */
-  //const auto words = readWords("/usr/share/dict/words");
-  /*
-   *Print out the number of palindromic words in this container
-   */
-   std::cout << "Hello" << std::endl;
-  // std::cout << std::count_if(words.begin(),
-                             //words.end(),
-                             //[](const std::string & word) {
-                              //return isPalindromic(word);
-                              //}) << std::endl;;
-  /*
-   *Note: this is a suggested function signature, feel free to implement
-   whatever you see fit!
-   */
+
+    //--------------------------------------------------------------------------
+    // Ask to insert file
+    std::string filePath = inserPathToFile();
+
+    //--------------------------------------------------------------------------
+    // Reading the all the words in a text file. The words are separated
+    // by spaces
+    const auto words = readWords(filePath);
+
+    // count the palindromic words in the file
+    std::cout << "Total number of palindromic words in the file: " << std::count_if( words.begin(),
+                                words.end(),
+                                [](const std::string & word) { return isPalindromic(word); }
+                              ) << std::endl;
+
+    // print palindromic words
+    std::cout << "------------------------------------------------" << std::endl;
+    for (int i = 0; i < words.size(); ++i)
+        if (isPalindromic(words[i]))
+            std::cout << "This is a Palindromic Word:    " << words[i] << std::endl;
+
+    return 0;
+
 }
