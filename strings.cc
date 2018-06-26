270aa248bc36787a54e781555d684989e6649be6
ZacharyIG <zacharyigreenberg@gmail.com>
2018-06-25 20:29:14 -0700

    Update strings.cc

--- strings.cc
+++ strings.cc
@@ -44,7 +44,6 @@ bool isPalindromic( const std::string word ) {
   }
 
   else {
-
     for (int i = 0; i < (word.length() / 2); i++) {
       if (word[i] != word[(word.length() - 1 - i)]) {
 	return false;
