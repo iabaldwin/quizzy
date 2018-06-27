a18788f7dd0d08e4645e033ee240e93bf7d060be
Patrick Doran <patjdor@gmail.com>
2018-06-26 20:42:57 -0400

    Implement strings.cc

--- strings.cc
+++ strings.cc
@@ -1,23 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <assert.h>
+#include <cassert>
+#include <algorithm>
+
+static const std::string DICTIONARY_PATH = "/usr/share/dict/words";
+
+/**
+ * @brief Return true if the string is a palindrome, false otherwise.
+ * @details Return true if the string is a palindrome. No verification is done
+ * to determine if the input string is a word (e.g. "N" is a palindrome, but is
+ * "N" a word?). All characters in the string are treated as part of the word; 
+ * e.g. "a man a plan a canal panama" is a palindromic sentence, but only
+ * after adjusting the spacing.
+ * 
+ * The solution is O(N) in the number of characters. It uses std::equal to 
+ * compare the first half of the word to the second half in reverse order.
+ * @see std::equal
+ */
+inline bool isPalindromic(const std::string& word){
+    return std::equal(word.begin(), word.begin()+word.size()/2, word.rbegin());
+}
 
 int main() {
-  /*
-   *Read in the system dictionary, and print out the number of palindromic words
-   */
-  //const auto words = readWords("/usr/share/dict/words");
-  /*
-   *Print out the number of palindromic words in this container
-   */
-  //std::cout << std::count_if(words.begin(),
-                             //words.end(),
-                             //[](const std::string & word) {
-                              //return isPalindromic(word);
-                              //}) << std::endl;;
-  /*
-   *Note: this is a suggested function signature, feel free to implement
-   whatever you see fit!
-   */
+    // Read the dictionary file line by line because it could be too large to 
+    // fit in memory. On Ubuntu 18.04 there are 102305 lines each containing 
+    // one word.
+    std::ifstream fs(DICTIONARY_PATH);
+    std::string line;
+    uint64_t count = 0;
+    while (std::getline(fs, line)){
+        if (isPalindromic(line)){
+            count++;
+        }
+    }
+    std::cout<<count<<std::endl;
+    return 0;
 }
