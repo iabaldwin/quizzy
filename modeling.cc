bf6c17269c3cf84d261dd64182c4e3aa4ca5ab4d
Ken Siebert <siebertken@yahoo.com>
2018-06-29 19:55:25 -0700

    linear regression for modeling

--- modeling.cc
+++ modeling.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <numeric>
 #include <array>
 
 constexpr int kDimensions = 2;
@@ -26,15 +27,52 @@ constexpr std::array<std::array<float, kDimensions>, kDataPoints> points = {{{3.
                                                                              {1.9357873494944466, 2.9778798335036534}}};
 
 
+std::pair<float, float> getLinearRegressionCoeffs(std::array<std::array<float, kDimensions>, kDataPoints> data_points) {
+    float x_sum = std::accumulate(data_points.begin(), data_points.end(), 0.0f, [](auto lhs, const auto rhs) {return lhs + rhs[0];});
+    float y_sum = std::accumulate(data_points.begin(), data_points.end(), 0.0f, [](auto lhs, const auto rhs) {return lhs + rhs[1];});
+    float x_mean = x_sum / static_cast<float>(kDataPoints);
+    float y_mean = y_sum / static_cast<float>(kDataPoints);
+
+    float xy_covariance = 0.0f;
+    float x_variance = 0.0f;
+    float x, y;
+    for (auto data_point : data_points) {
+        x = data_point[0];
+        y = data_point[1];
+        x_variance += std::pow(x - x_mean, 2);
+        xy_covariance += ((y - y_mean) * (x - x_mean));
+    }
+
+    float beta = xy_covariance / x_variance;
+    float alpha = y_mean - (beta * x_mean);
+
+    return std::pair<float, float>(alpha, beta);
+}
+
+float getLinearRegressionMse(std::array<std::array<float, kDimensions>, kDataPoints> data_points, std::pair<float, float> coeffs) {
+    float mse = 0.0f;
+    float total_data_points_f = static_cast<float>(data_points.size());
+    float pred, label, x;
+
+    for (auto data_point : data_points) {
+        x = data_point[0];
+        label = data_point[1];
+
+        pred = coeffs.first + (x * coeffs.second);
+        mse += std::pow(pred - label, 2) / total_data_points_f;
+        std::cout << "pred = " << pred << ", label = " << label << std::endl;
+    }
+
+    return mse;
+}
+
 int main() {
   /*
    *Use a model of your choice to approximate the relationship between the
    independent/dependent variable pairs above. Model choice is up to you - just
    print out coefficients!
    */
-  //std::cout << model(points) << std::endl;
-  /*
-   *Note: this is a suggested function signature, feel free to implement
-   whatever you see fit!
-   */
+  auto lr_coeffs = getLinearRegressionCoeffs(points);
+  auto mse = getLinearRegressionMse(points, lr_coeffs);
+  std::cout << "alpha = " << lr_coeffs.first << ", beta = " << lr_coeffs.second << ", mse = " << mse << std::endl;
 }
