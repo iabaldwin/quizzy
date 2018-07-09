813da1b1882afdef2794ffe4afe11ab951efd574
Michael Wang <mswang12@gmail.com>
2018-07-08 18:27:39 -0700

    Coding exercises

--- entropy.cc
+++ entropy.cc
@@ -1,10 +1,51 @@
 #include <cmath>
 #include <iostream>
 #include <fstream>
+#include <random>
+
+int frequency(std::vector<int> array, int target) {
+  // Returns the frequency of the target
+  int count = 0;
+  for (std::vector<int>::iterator it = array.begin(); it != array.end(); ++it) {
+    if (*it == target) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 template <typename NumericType, int Discretization>
-NumericType entropy(const std::string& fileDescriptor) {
-  throw std::runtime_error("Implement me!");
+NumericType entropy() {
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  std::default_random_engine generator(seed);
+  
+  size_t max_value = 128 - 1;
+  size_t num_events = max_value / Discretization;
+  std::uniform_int_distribution<int> distribution(0, max_value);
+
+  // Generate a series of dice rolls between 0 and the max_value
+  size_t num_rolls = 100;
+  std::vector<int> array;
+  for (size_t i = 0; i < num_rolls; ++i) {
+    int raw_rand = distribution(generator);
+    int dice_roll = raw_rand / Discretization;
+    array.push_back(dice_roll);
+  }
+
+  // Calculate the entropy
+  NumericType entropy = 0.;
+  for (size_t i = 0; i < num_events; ++i) {
+    int count = frequency(array, i);
+    if (count == 0) {
+      continue;
+    }
+
+    NumericType p = NumericType(count) / num_rolls;
+    entropy -= p * log2(p);
+  }
+
+  return entropy;
 }
 
 int main() {
@@ -12,7 +53,7 @@ int main() {
    *Print out the entropy of a probability distribution over 8 discrete events,
    with event occurrences given by a random source of information. 
    */
-  //std::cout << entropy<float, 8>("/dev/random") << std::endl;
+  std::cout << "Entropy in bits: " << entropy<float, 8>() << std::endl;
   /*
    *Note: this is a suggested function signature, feel free to implement
    whatever you see fit!
--- strings.cc
+++ strings.cc
@@ -3,19 +3,39 @@
 #include <vector>
 #include <assert.h>
 
+void readWords(char const* file_path, std::vector<std::string> *words) {
+  std::ifstream input(file_path);
+  std::string line;
+
+  while (getline(input, line)) {
+    words->push_back(line);
+  }
+}
+
+bool isPalindromic(std::string word) {
+  for (size_t i = 0; i < word.size(); ++i) {
+    size_t j = word.size() - 1 - i;
+    if (word[i] != word[j]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 int main() {
   /*
    *Read in the system dictionary, and print out the number of palindromic words
    */
-  //const auto words = readWords("/usr/share/dict/words");
+  std::vector<std::string> words;
+  readWords("/usr/share/dict/words", &words);
   /*
    *Print out the number of palindromic words in this container
    */
-  //std::cout << std::count_if(words.begin(),
-                             //words.end(),
-                             //[](const std::string & word) {
-                              //return isPalindromic(word);
-                              //}) << std::endl;;
+  std::cout << std::count_if(words.begin(),
+                             words.end(),
+                             [](const std::string & word) {
+                              return isPalindromic(word);
+                              }) << std::endl;;
   /*
    *Note: this is a suggested function signature, feel free to implement
    whatever you see fit!
