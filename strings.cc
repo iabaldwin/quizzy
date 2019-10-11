9941df515b02668aed5deb701af914cbf1bb63de
Achyut Boggaram <achbogga@gmail.com>
2019-10-10 19:10:11 -0700

    functional strings

--- strings.cc
+++ strings.cc
@@ -2,22 +2,40 @@
 #include <fstream>
 #include <vector>
 #include <assert.h>
+#include <string>
+#include <streambuf>
+#include <sstream>
+#include <algorithm>
+
+//read words from a given filename
+std::vector< std::string > readWords(const std::string& filename){
+	std::ifstream t(filename);
+	std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+    std::string tmp;
+    std::stringstream str_strm(str);
+    std::vector< std::string > words;
+    
+    while (str_strm >> tmp) {
+        if (!tmp.empty()) words.push_back(tmp);
+    }
+   	return words;
+}
+
+//check if a given word is a palindrome
+bool isPalindromic(const std::string& s){
+	return equal(s.begin(), s.begin() + s.size()/2, s.rbegin());
+}
 
 int main() {
   /*
    *Read in the system dictionary, and print out the number of palindromic words
    */
-  //const auto words = readWords("/usr/share/dict/words");
+	const auto words = readWords("/usr/share/dict/words");
   /*
    *Print out the number of palindromic words in this container
    */
-  //std::cout << std::count_if(words.begin(),
-                             //words.end(),
-                             //[](const std::string & word) {
-                              //return isPalindromic(word);
-                              //}) << std::endl;;
-  /*
-   *Note: this is a suggested function signature, feel free to implement
-   whatever you see fit!
-   */
+	std::cout << std::count_if(words.begin(), words.end(), [](const std::string & word) {
+    															return isPalindromic(word);
+    														}) << std::endl;;
+   return 0;
 }
